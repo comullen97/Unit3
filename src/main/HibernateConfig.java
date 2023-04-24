596dd083b0f79d67a0ff1d6fceb4b690e14a4eb2
Casey Mullen <comullen97@gmail.com>
2023-04-23 22:24:05 -0700

    First commit

---
+++ src/main/HibernateConfig.java
@@ -0,0 +1,12 @@
+@Configuration
+public class HibernateConfig {
+
+
+    public SessionFactory getFactory() {
+        SessionFactory factory = new org.hibernate.cfg.Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(MovieEntity.class)
+                .addAnnotatedClass(VoteEntity.class)
+                .buildSessionFactory();
+        return factory;
+    }
